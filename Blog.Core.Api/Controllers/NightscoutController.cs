ce2d9f9041bc18f3c1af0bc0bcc2734705f34eed
hudingwen <765472804@qq.com>
2023-07-11 00:48:02 +0800

    fix

--- Blog.Core.Api/Controllers/NightscoutController.cs
+++ Blog.Core.Api/Controllers/NightscoutController.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Core.Common;
 using Blog.Core.Common.Extensions;
+using Blog.Core.Common.Helper;
 using Blog.Core.IServices;
 using Blog.Core.IServices.BASE;
 using Blog.Core.Model;
@@ -9,11 +10,12 @@ using Blog.Core.Repository.UnitOfWorks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
-using MongoDB.Driver.Core.Servers;
 using Newtonsoft.Json;
 using Serilog;
+
 using SqlSugar;
 using System.Linq.Expressions;
+using System.Net.Http.Headers;
 
 namespace Blog.Core.Api.Controllers
 {
@@ -212,7 +214,7 @@ namespace Blog.Core.Api.Controllers
                     if (string.IsNullOrEmpty(request.url))
                     {
                         var templateUrl = AppSettings.app(new string[] { "nightscout", "TemplateUrl" }).ObjToString();
-                        request.url = string.Format(templateUrl, GenerateNumber(2) + padName);
+                        request.url = string.Format(templateUrl, GenerateNumber(3) + padName);
                     }
 
                     
@@ -534,7 +536,7 @@ namespace Blog.Core.Api.Controllers
         public async Task<MessageModel<string>> updateNsServer([FromBody] NightscoutServer request)
         {
             var data = await _nightscoutServerServices.QueryById(request.Id);
-            if(data ==null) MessageModel<string>.Success("要更新的数据不存在");
+            if(data ==null) MessageModel<string>.Fail("要更新的数据不存在");
             var id = await _nightscoutServerServices.Update(request);
             return MessageModel<string>.Success("添加成功", id.ObjToString());
         }
@@ -563,7 +565,364 @@ namespace Blog.Core.Api.Controllers
         }
 
 
+        /// <summary>
+        /// 获取小程序绑定二维码
+        /// </summary>
+        /// <param name="nsid"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetBindQR(long nsid)
+        {
+            var appid = AppSettings.app(new string[] { "miniProgram", "appid" }).ObjToString();
+            var secret = AppSettings.app(new string[] { "miniProgram", "secret" }).ObjToString();
+            var pushCompanyCode = AppSettings.app(new string[] { "nightscout", "pushCompanyCode" }).ObjToString();
+            string url = $"https://api.weixin.qq.com/cgi-bin/stable_token";
+
+            var weChatToken = new { appid = appid, secret = secret, grant_type = "client_credential" };
+
+            var nsInfo =  await _nightscoutServices.QueryById(nsid);
+
+
+
+            WeChatQR weChatQR = new WeChatQR
+            {
+                QRbindCompanyID = pushCompanyCode,
+                QRbindJobID = nsInfo.Id.ObjToString(),
+                QRbindJobNick = nsInfo.name,
+                QRcrateTime = DateTime.Now,
+                QRpublicAccount = appid,
+                QRticket = StringHelper.GetGUID()
+            };
+
+            await _weChatConfigServices.Db.Insertable<WeChatQR>(weChatQR).ExecuteCommandAsync();
+
+            string result;
+            using (HttpContent httpContent = new StringContent(JsonHelper.ObjToJson(weChatToken)))
+            {
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.Timeout = new TimeSpan(0, 0, 60);
+                    result = await httpClient.PostAsync(url, httpContent).Result.Content.ReadAsStringAsync();
+                    AccessTokenDto accessTokenDto = JsonHelper.JsonToObj<AccessTokenDto>(result);
+
+                   
+                    var ticket = weChatQR.QRticket;
+                    var jsonBind = JsonHelper.ObjToJson(new { path = $"pages/index/index?ticket={ticket}", env_version = "develop", width = 128 });
+                    using (HttpContent httpContentBind = new StringContent(jsonBind))
+                    {
+                        var urlBind = $"https://api.weixin.qq.com/wxa/getwxacode?access_token={accessTokenDto.access_token}";
+                        var bindstream = await httpClient.PostAsync(urlBind, httpContentBind).Result.Content.ReadAsStreamAsync();
+                        return File(bindstream, "image/jpeg");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 绑定小程序
+        /// <param name="ticket"></param>
+        /// <param name="openid"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<MessageModel<string>> BindQR(string ticket,string openid)
+        {
+
+            string appid = AppSettings.app(new string[] { "miniProgram", "appid" }).ObjToString();
+            var pushCompanyCode = AppSettings.app(new string[] { "nightscout", "pushCompanyCode" }).ObjToString();
+            var ticketInfo = await _weChatConfigServices.Db.Queryable<WeChatQR>().Where(t => t.QRpublicAccount == appid && t.QRticket == ticket && t.QRisUsed == false).FirstAsync();
+            if (ticketInfo == null)
+               return MessageModel<string>.Fail($"没有找到ticket:{ticket},请勿重复使用");
+            var bindInfo = await _weChatConfigServices.Db.Queryable<WeChatSub>().Where(t => t.SubFromPublicAccount == appid && t.CompanyID == pushCompanyCode && t.SubJobID == ticketInfo.QRbindJobID).FirstAsync();
+
+            _unitOfWorkManage.BeginTran();
+           try
+            {
+                if (bindInfo == null)
+                {
+                    bindInfo = new WeChatSub
+                    {
+                        SubFromPublicAccount = appid,
+                        SubJobID = ticketInfo.QRbindJobID,
+                        SubUserOpenID = openid,
+                        CompanyID = ticketInfo.QRbindCompanyID,
+                        SubUserRegTime = DateTime.Now,
+                        IsUnBind = false
+                    };
+
+                    await _weChatConfigServices.Db.Insertable<WeChatSub>(bindInfo).ExecuteCommandAsync();
+                }
+                else
+                {
+
+                    bindInfo.LastSubUserOpenID = bindInfo.SubUserOpenID;
+                    //bindInfo.SubFromPublicAccount = appid;
+                    bindInfo.SubJobID = ticketInfo.QRbindJobID;
+                    //bindInfo.SubUserOpenID = openid;
+                    //bindInfo.CompanyID = ticketInfo.QRbindCompanyID;
+                    bindInfo.SubUserRefTime = DateTime.Now;
+                    bindInfo.IsUnBind = false;
+                    await _weChatConfigServices.Db.Updateable<WeChatSub>(bindInfo).UpdateColumns(t => new {t.LastSubUserOpenID ,t.SubJobID ,t.SubUserRefTime ,t.IsUnBind }).ExecuteCommandAsync();
+                }
+                ticketInfo.QRisUsed = true;
+                ticketInfo.QRuseTime = DateTime.Now;
+                ticketInfo.QRuseOpenid = openid;
+                await _weChatConfigServices.Db.Updateable<WeChatQR>(ticketInfo).UpdateColumns(t=>new {t.QRisUsed,t.QRuseOpenid,t.QRuseTime }).ExecuteCommandAsync();
+                _unitOfWorkManage.CommitTran();
+            }
+            catch (Exception)
+            {
+                _unitOfWorkManage.RollbackTran();
+                throw;
+            }
+            return MessageModel<string>.Success("绑定成功");
+        }
+        /// <summary>
+        /// 取消绑定小程序
+        /// </summary>
+        /// <param name="appid"></param>
+        /// <param name="nsid"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<MessageModel<string>> UnBindQR(string appid, string nsid)
+        {
+            var pushCompanyCode = AppSettings.app(new string[] { "nightscout", "pushCompanyCode" }).ObjToString();
+            var bindInfo = await _weChatConfigServices.Db.Queryable<WeChatSub>().Where(t => t.SubFromPublicAccount == appid && t.SubJobID == nsid && t.CompanyID == pushCompanyCode && t.IsUnBind == false).ToListAsync();
+            if(bindInfo == null || bindInfo.Count== 0)
+                return MessageModel<string>.Fail("没有找到解绑信息");
+            foreach (var item in bindInfo)
+            {
+                item.IsUnBind = true;
+                item.SubUserRefTime = DateTime.Now;
+            }
+            await  _weChatConfigServices.Db.Updateable< WeChatSub>(bindInfo).UpdateColumns(t => new {t.IsUnBind,t.SubUserRefTime}).ExecuteCommandAsync();
+            return MessageModel<string>.Success("解绑成功");
+        }
+        /// <summary>
+        /// 小程code换openid
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<MessageModel<WeChatModel>> CodeLogin(string code)
+        {
+            var appid = AppSettings.app(new string[] { "miniProgram", "appid" }).ObjToString();
+            var secret = AppSettings.app(new string[] { "miniProgram", "secret" }).ObjToString();
+            HttpClient httpClient = new HttpClient();
+            var res = await httpClient.GetStringAsync($"https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type=authorization_code");
+
+            var data = JsonHelper.JsonToObj<WeChatModel>(res);
+            if (data.errcode.Equals(0))
+            {
+                return MessageModel<WeChatModel>.Success(string.Empty, data);
+            }
+            else
+            {
+                return MessageModel<WeChatModel>.Fail(string.Empty, data);
+            }
+        }
+        /// <summary>
+        /// 小程序获取血糖信息
+        /// </summary>
+        /// <param name="openid"></param>
+        /// <returns></returns>
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<MessageModel<SugarDTO>> GetCurBloodSugar(string openid)
+        {
+            SugarDTO sugarDTO = new SugarDTO();
+            var appid = AppSettings.app(new string[] { "miniProgram", "appid" }).ObjToString();
+            var Host = AppSettings.app(new string[] { "miniProgram", "Host" }).ObjToString();
+            var Port = AppSettings.app(new string[] { "miniProgram", "Port" }).ObjToString();
+            var LoginName = AppSettings.app(new string[] { "miniProgram", "LoginName" }).ObjToString();
+            var LoginPasswd = AppSettings.app(new string[] { "miniProgram", "LoginPasswd" }).ObjToString();
+            var pushCompanyCode = AppSettings.app(new string[] { "nightscout", "pushCompanyCode" }).ObjToString();
+
+            //configuration.GetSection("sayings").Get<string[]>();
+            List<string> sayings = AppSettings.app<string>(new string[] { "miniProgram", "sayings" });
+            var title = AppSettings.app(new string[] { "miniProgram", "title" }).ObjToString();
+
+            var nsid = await _weChatConfigServices.Db.Queryable<WeChatSub>().Where(t=>t.SubFromPublicAccount == appid && t.CompanyID == pushCompanyCode && t.SubUserOpenID == openid && t.IsUnBind == false).Select(t=>t.SubJobID).FirstAsync();
+
+            if (string.IsNullOrEmpty(nsid))
+                return MessageModel<SugarDTO>.Fail("无绑定信息,无法查看血糖");
+            var longNsid = nsid.ObjToLong();
+            var serviceName = await _weChatConfigServices.Db.Queryable<Nightscout>().Where(t => t.Id == longNsid).Select(t=>t.serviceName).FirstAsync();
+            if (string.IsNullOrEmpty(serviceName))
+                return MessageModel<SugarDTO>.Fail("无血糖信息可供查看,请检查是否绑定NS");
+
+            var grantConnectionMongoString = $"mongodb://{LoginName}:{LoginPasswd}@{Host}:{Port}";
+            var client = new MongoClient(grantConnectionMongoString);
+            var database = client.GetDatabase(serviceName);
+            var collection = database.GetCollection<MongoDB.Bson.BsonDocument>("entries"); // 替换为你的集合名称
+
+            var filter = Builders<MongoDB.Bson.BsonDocument>.Filter.Empty; // 获取所有数据
+            var projection = Builders<MongoDB.Bson.BsonDocument>.Projection.Include("date").Include("sgv").Include("direction").Exclude("_id");
+
+
+            var ls = await collection.Find(filter).Sort(Builders<MongoDB.Bson.BsonDocument>.Sort.Descending("_id")).Limit(900).Project(projection).ToListAsync();
+
+
+            var sugers = JsonHelper.JsonToObj<List<EntriesEntity>>(ls.ToJson());
+            if (sugers == null) sugers = new List<EntriesEntity>();
+            foreach (var item in sugers)
+            {
+                FormatDate(item);
+            }
+
+            sugarDTO.curBlood = sugers.Count > 0 ? sugers[0] : new EntriesEntity { date_str = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") };
+            sugarDTO.curBlood.title = title;
+            Random rd = new Random();
+
+            if (sayings != null && sayings.Count > 0)
+            {
+                sugarDTO.curBlood.saying = sayings[rd.Next(0, sayings.Count)];
+            }
+            else
+            {
+                sugarDTO.curBlood.saying = "每一次在控制血糖上的成功都是向自己付出的最好回报。";
+            }
+
+            {
+                //今天
+                var flagDate = DateTime.Now.Date;
+                sugarDTO.day0 = HandleSugarList(sugers, flagDate);
+
+            }
+            {
+                //昨天
+                var flagDate = DateTime.Now.Date.AddDays(-1);
+                sugarDTO.day1 = HandleSugarList(sugers, flagDate);
+
+            }
+            {
+                //前天
+                var flagDate = DateTime.Now.Date.AddDays(-2);
+                sugarDTO.day2 = HandleSugarList(sugers, flagDate);
+
+            }
+            return MessageModel<SugarDTO>.Success("", sugarDTO);
+
+        }
+
+        private List<EntriesEntity> HandleSugarList(List<EntriesEntity> sugers, DateTime flagDate)
+        {
+            List<EntriesEntity> flagList = sugers.Where(t => t.date_now.Date == flagDate).ToList();
+            //flagList.Reverse();
+
+            var ls = new List<EntriesEntity>();
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(0).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(3).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(6).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(9).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(12).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(15).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(18).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(21).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            ls.Add(new EntriesEntity { date = (decimal)(flagDate.AddHours(23).AddMinutes(59).AddSeconds(59).AddHours(-8) - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds, isMask = true });
+            foreach (var item in ls)
+            {
+                FormatDate(item);
+            }
+            flagList.AddRange(ls);
+            return flagList.AsEnumerable().OrderBy(s => s.date_now).ToList();
+        }
+
+
+        private void FormatDate(EntriesEntity dd)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds((long)dd.date);
+            DateTime dateTime = dateTimeOffset.UtcDateTime.AddHours(8);
+            dd.date_now = dateTime;
+            dd.date_str = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            dd.date_time = dateTime.ToString("HH:mm:ss");
+
+            dd.date_step = (int)(DateTime.Now - dateTime).TotalMinutes;
+
+            if (dd.sgv != null)
+                dd.sgv_str = Math.Round(1.0 * dd.sgv.Value / 18, 1);
+
+
+            dd.direction_str = GetFlag(dd.direction);
+        }
+        private string GetFlag(string flag)
+        {
+            switch (flag)
+            {
+                case "NONE":
+                    return "⇼";
+                case "TripleUp":
+                    return "⤊";
+                case "DoubleUp":
+                    return "⇈";
+                case "SingleUp":
+                    return "↑";
+                case "FortyFiveUp":
+                    return "↗";
+                case "Flat":
+                    return "→";
+                case "FortyFiveDown":
+                    return "↘";
+                case "SingleDown":
+                    return "↓";
+                case "DoubleDown":
+                    return "⇊";
+                case "TripleDown":
+                    return "⤋";
+                case "NOT COMPUTABLE":
+                    return "-";
+                case "RATE OUT OF RANGE":
+                    return "⇕";
+                default:
+                    return "未知";
+            }
+        }
+
+
     }
 
+    public class SugarDTO
+    {
+        public List<EntriesEntity> day0 { get; set; }
+
+        public List<EntriesEntity> day1 { get; set; }
+
+        public List<EntriesEntity> day2 { get; set; }
+
+        public EntriesEntity curBlood { get; set; }
+
+    }
+
+    public class WeChatModel
+    {
+        public string openid { get; set; }
+        public string session_key { get; set; }
+        public string unionid { get; set; }
+        public int errcode { get; set; }
+        public string errmsg { get; set; }
+    }
+
+    public class EntriesEntity
+    {
+        public decimal date { get; set; }
+
+        public DateTime date_now { get; set; }
+        public string date_str { get; set; }
+        public string date_time { get; set; }
+        public int? date_step { get; set; }
+        public int? sgv { get; set; }
+        public double? sgv_str { get; set; }
+        public string direction { get; set; }
+        public string direction_str { get; set; }
+        public bool isMask { get; set; }
+        public string title { get; set; }
+        public string saying { get; set; }
+    }
+    public class AccessTokenDto
+    {
+        public string access_token { get; set; }
+    }
 
 }
